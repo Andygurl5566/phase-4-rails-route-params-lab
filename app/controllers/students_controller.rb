1858a08d648c5d54086d5cb6318083f7f78327c0
Andrea Freeman <afreeman5566@gmail.com>
2021-11-22 09:29:39 -0600

    commit tree ce4e842eb17a90e385d68cf8039717cb0e01c8ed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,21 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades
+    students = Student.all.order(grade: :desc)
+    render json: students
+end
+
+def highest_grade
+    students = Student.all.max_by do |element|
+        element.grade
+    end
+    render json: students
+end
+
+  def show
+    students = Student.find(params[:id])
+    render json: students
+  end
+
 end
